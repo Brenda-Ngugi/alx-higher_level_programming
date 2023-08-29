bae489701949da81b6444cf1e479f8c345727ade
Brenda-Ngugi <brendangugi7@gmail.com>
2023-08-29 13:13:01 -0700

    oop

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Defines a square with a given size.
+
+    Attributes:
+        __size (int): The size of the square
+"""
+
+
+class Square:
+    """Initializes the square object """
+    def __init__(self, size):
+        self.__size = size
